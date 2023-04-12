edfc7bd3f9a924d7f3f6911583583b3fa7072827
grego <kb.grego@gmail.com>
2023-04-12 21:33:00 +0300

    Style chages

--- src/app/cart-groups/cart-groups.component.ts
+++ src/app/cart-groups/cart-groups.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Schedule } from '../schedule';
 import { Logger } from '../logger.service';
 import { ScheduleService } from '../schedule.service';
+import { SettingsService } from '../settings.service';
+import { Witness } from '../witness';
 
 
 @Component({
@@ -15,7 +17,9 @@ export class CartGroupsComponent implements OnInit {
 
   @Input() isNew: boolean = false;
 
-  constructor(private scheduleService: ScheduleService) { }
+  witnesses: String[] = [];
+
+  constructor(private scheduleService: ScheduleService, private settings: SettingsService) { }
 
 
   onChange(value: boolean) {
@@ -27,8 +31,20 @@ export class CartGroupsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    this.witnesses = this.getNameList()
+                          .filter(w => w.activated)
+                          .map(w => w.name)
+                          .sort((a, b) => a.localeCompare(b));
   }
+
+  getNameList(): Witness[] {
+    let witn = this.settings.getWitnesses();
+    witn.filter(w => w.activated)
+        .sort((a, b) => a.name.localeCompare(b.name));
+
+   return witn;
+ }
+
 
   onClickRemove(schedule: Schedule): void {
     this.scheduleService.delete(schedule);
